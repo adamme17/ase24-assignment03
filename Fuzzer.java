b7adf1d30ddefb770118f4695a2b0b0f617c63f3
Adam <adambokun@gmail.com>
2024-11-29 21:03:29 +0100

    assigmnent 3

--- Fuzzer.java
+++ Fuzzer.java
@@ -1,8 +1,7 @@
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -25,10 +24,7 @@ public class Fuzzer {
         ProcessBuilder builder = getProcessBuilderForCommand(commandToFuzz, workingDirectory);
         System.out.printf("Command: %s\n", builder.command());
 
-        runCommand(builder, seedInput, getMutatedInputs(seedInput, List.of(
-                input -> input.replace("<html", "a"), // this is just a placeholder, mutators should not only do hard-coded string replacement
-                input -> input.replace("<html", "")
-        )));
+        runCommand(builder, seedInput, getMuatedInputs(seedInput));
     }
 
     private static ProcessBuilder getProcessBuilderForCommand(String command, String workingDirectory) {
@@ -40,14 +36,88 @@ public class Fuzzer {
             builder.command("sh", "-c", command);
         }
         builder.directory(new File(workingDirectory));
-        builder.redirectErrorStream(true); // redirect stderr to stdout
+        builder.redirectErrorStream(true);
         return builder;
     }
 
     private static void runCommand(ProcessBuilder builder, String seedInput, List<String> mutatedInputs) {
-        Stream.concat(Stream.of(seedInput), mutatedInputs.stream()).forEach(
-                input -> { }
-        );
+        Stream.concat(Stream.of(seedInput), mutatedInputs.stream()).forEach(input -> {
+            try {
+                Process process = builder.start();
+                try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()))) {
+                    writer.write(input);
+                    writer.flush();
+                    writer.close();
+                }
+
+                int exit = process.waitFor();
+                String output = readStreamIntoString(process.getInputStream());
+                if (exit != 0) {
+                    System.out.printf("Input:\n%s\nOutput:\n%s\n", input, output);
+                    System.out.printf("Exit code: %s\n", exit);
+                    System.out.printf("\n ");
+
+                    System.exit(exit);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public static List<String> getMuatedInputs(String seedInput) {
+        List<String> inputs = new ArrayList<String>();
+
+        inputs.add(mutateWithExcessiveLength(seedInput));
+
+        for (int i = 0; i < 20; i++) {
+            inputs.add(mutateWithSpecialChars(seedInput));
+            inputs.add(mutateDeleteChars(seedInput));
+            inputs.add(mutateWithMalformedHtml(seedInput));
+            inputs.add(mutateWithInvalidChars(seedInput));
+            inputs.add(mutateDuplicateChar(seedInput));
+        }
+
+        return inputs;
+    }
+
+    public static String mutateWithExcessiveLength(String input) {
+        StringBuilder longString = new StringBuilder("A".repeat(100));
+        return input.replace("value", longString);
+    }
+
+    public static String mutateWithSpecialChars(String input) {
+        return input.repeat(100);
+    }
+
+    public static String mutateDeleteChars(String input) {
+        Random random = new Random();
+        StringBuilder mutated = new StringBuilder(input);
+        int position = random.nextInt(input.length());
+        int randomLen = random.nextInt(input.length() - position) + 1;
+        mutated.delete(position, position + randomLen);
+        return mutated.toString();
+    }
+
+    public static String mutateWithMalformedHtml(String input) {
+        Random random = new Random();
+        StringBuilder mutatedInput = new StringBuilder(input);
+        int position = random.nextInt(input.length() + 1);
+        return mutatedInput.insert(position, ">>".repeat(100)).toString();
+    }
+
+    public static String mutateWithInvalidChars(String input) {
+        return input.replace("value", "\0\0\0");
+    }
+
+    public static  String mutateDuplicateChar(String input) {
+        Random random = new Random();
+        StringBuilder mutated = new StringBuilder(input);
+        char charToDuplicate = mutated.charAt(random.nextInt(input.length()));
+        String chars = String.valueOf(charToDuplicate).repeat(100);
+        int position = random.nextInt(input.length());
+        mutated.insert(position, chars);
+        return mutated.toString();
     }
 
     private static String readStreamIntoString(InputStream inputStream) {
@@ -58,6 +128,15 @@ public class Fuzzer {
     }
 
     private static List<String> getMutatedInputs(String seedInput, Collection<Function<String, String>> mutators) {
-        return List.of();
+        return mutators.stream()
+                .map(mutator -> mutator.apply(seedInput))
+                .collect(Collectors.toList());
+    }
+
+    private static String switchCase(String input) {
+        return input.chars()
+                .mapToObj(c -> Character.isUpperCase(c) ? Character.toLowerCase((char) c) : Character.toUpperCase((char) c))
+                .map(Object::toString)
+                .collect(Collectors.joining());
     }
 }
